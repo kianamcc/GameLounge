01c7eb4e351d0352d36055e8ce967baab730f29d
kianamcc <42532725+kianamcc@users.noreply.github.com>
2022-09-06 21:57:22 -0700

    navbar, navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/gamelounge-logo.png";
+import user from "../../assets/user.png";
 
 const Navbar = () => {
   return (
     <div className="navbar" data-testid="nav">
-      Navbar
+      <div className="navbar-items">
+        <img className="logo" src={logo} alt="" />
+        <h2 className="games">Games</h2>
+        <h2 className="about">About</h2>
+        <h2 className="about">Contact</h2>
+        <img className="user" src={user} alt="" />
+      </div>
     </div>
   );
 };
