031fff70ec795f94881ca8e5f0e538382b651ca6
kianamcc <42532725+kianamcc@users.noreply.github.com>
2022-09-08 13:52:27 -0700

    Initialize Popular Games component with mock data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import PopularGames from "./components/popular-games/PopularGames";
+import mockData from "./data";
 
 function App() {
+  // states
+  const [data, setData] = useState(mockData);
+  console.log(mockData);
+  // fetch API
+
   return (
     <div className="App">
       <Navbar />
-      <PopularGames />
+      <PopularGames data={data} />
     </div>
   );
 }
--- src/components/popular-games/PopularGames.jsx
+++ src/components/popular-games/PopularGames.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./PopularGames.css";
+import liquid from "../../assets/liquid.jpeg";
 
-const PopularGames = () => {
-  return <div className="popular-games">Popular Games</div>;
+const PopularGames = (props) => {
+  return (
+    <div className="popular-games">
+      <h2 className="popular-games-header">Popular Games</h2>
+      <div className="popular-games-card-container">
+        {props.data.map((game) => {
+          return (
+            <div className="popular-game-card" key={game.id}>
+              <img src={liquid} alt="" className="popular-game-img" />
+              <h3 className="popular-game-name">{game.name}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PopularGames;
